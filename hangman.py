50416cfa086cb22fa6db58ca311b3aa3f7a94b4f
hemera <hemerocallisongml@gmail.com>
2018-05-08 21:06:06 +0700

    Hangman game is done

--- hangman.py
+++ hangman.py
@@ -1,4 +1,4 @@
-import random
+п»їimport random
 
 def hangman(word):
     wrong = 0
@@ -15,11 +15,11 @@ def hangman(word):
     rletters = list(word)
     board = ["_"] * len(word)
     win = False
-    print("Добро пожаловать на казнь!")
+    print("Р”РѕР±СЂРѕ РїРѕР¶Р°Р»РѕРІР°С‚СЊ РЅР° РєР°Р·РЅСЊ!")
     
     while wrong < len(stages) - 1:
         print("\n")
-        msg = "Введите букву: "
+        msg = "Р’РІРµРґРёС‚Рµ Р±СѓРєРІСѓ: "
         char = input(msg)
         if char in rletters:
             cind = rletters.index(char)
@@ -31,13 +31,13 @@ def hangman(word):
         e = wrong + 1
         print("\n".join(stages[0: e]))
         if "_" not in board:
-            print("Вы выиграли! Было загадано слово: ")
+            print("Р’С‹ РІС‹РёРіСЂР°Р»Рё! Р‘С‹Р»Рѕ Р·Р°РіР°РґР°РЅРѕ СЃР»РѕРІРѕ: ")
             print("".join(board))
             win = True
             break
     if not win:
         print("\n".join(stages[0: e]))
-        print("Вы проиграли! Было загадано слово: {}".format(word))
+        print("Р’С‹ РїСЂРѕРёРіСЂР°Р»Рё! Р‘С‹Р»Рѕ Р·Р°РіР°РґР°РЅРѕ СЃР»РѕРІРѕ: {}".format(word))
         
 words = ["cat", "dog", "duck", "owl", "fox"]
 word = random.choice(words)
